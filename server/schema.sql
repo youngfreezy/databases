eb2c940be37a4c3defe694118ccecd12d7e327df
youngfreezy <Fareez.Ahmed@gmail.com>
2015-09-11 17:30:24 -0700

    implemeneted create table script for mysql and included picture of our db schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,39 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE Messages (
   /* Describe your table here.*/
+  M_id int NOT NULL,
+  text varchar(1000) NOT NULL,
+  created_at timestamp,
+  PRIMARY KEY (M_id)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE Users (
+  U_id int NOT NULL,
+  username varchar(100) NOT NULL,
+  PRIMARY KEY (U_id)
+);
 
+CREATE TABLE Rooms (
+  R_id int NOT NULL,
+  name varchar(20) NOT NULL,
+  PRIMARY KEY (R_id)
+);
 
+-- if you want to get rid of a referenced table below, first you need to get rid of this table, then drop the one above.  
 
+CREATE TABLE MessagesRoomsBridge (
+  MR_id int NOT NULL,
+  message_id int NOT NULL,
+  room_id int NOT NULL,
+  user_id int NOT NULL,
+  FOREIGN KEY(message_id) REFERENCES Messages(M_id),
+  FOREIGN KEY(room_id) REFERENCES Rooms(R_id),
+  FOREIGN KEY(user_id) REFERENCES Users(U_id),
+  PRIMARY KEY(MR_id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
